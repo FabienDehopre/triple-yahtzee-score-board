d6e3f35a09f86a70ec378e11877301ecb427e30e
Fabien Dehopré <fabien@dehopre.com>
2026-05-27 16:27:04 +0200

    chore: update import.meta.env structure

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,17 @@
 /// <reference types="vite/client" />
 
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  strictImportMetaEnv: unknown;
+}
+
 interface ImportMetaEnv {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
+  /* eslint-disable @typescript-eslint/naming-convention */
   readonly NG_APP_REPORT_ISSUE_ENDPOINT?: string;
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   readonly NG_APP_TURNSTILE_SITE_KEY?: string;
+  readonly VITEST?: boolean;
+  /* eslint-enable @typescript-eslint/naming-convention */
 }
 
 interface ImportMeta {
